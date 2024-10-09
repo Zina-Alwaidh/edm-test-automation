dcee97a68159edff5a379470eb3aa32046bcad73
OITHOUAlWaiZ <zina.alwaidh@va.gov>
2024-10-09 14:23:35 -0500

    locators and steps to sing in using IDme with inputting credentials of mock user

--- src/test/java/com/pages/loginPage.java
+++ src/test/java/com/pages/loginPage.java
@@ -1,5 +1,26 @@
 package com.pages;
 
-public class loginPage {
+import com.utlities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class loginPage {
+public loginPage() {
+    PageFactory.initElements((Driver.getDriver()), this);
+}
+@FindBy(xpath = "//button[@aria-label='Sign in with ID.me']")
+    public WebElement IDme;
+
+@FindBy(xpath = "//input[@id='user_email']")
+    public WebElement emailInput;
+
+@FindBy(xpath = "//input[@id='user_password']")
+    public WebElement passwordInput;
+
+@FindBy(xpath = "//input[@class='btn btn-primary']")
+    public WebElement signInButton;
+
+@FindBy(xpath = "//button[.='Continue']")
+    public WebElement continueButton;
 }
--- src/test/java/com/step_definitions/statementOfBenefitsHomePage_stepDefinitions.java
+++ src/test/java/com/step_definitions/statementOfBenefitsHomePage_stepDefinitions.java
@@ -1,5 +1,7 @@
 package com.step_definitions;
 
+import com.github.javafaker.Faker;
+import com.pages.loginPage;
 import com.utlities.ConfigurationReader;
 import com.utlities.Driver;
 import io.cucumber.java.en.Given;
@@ -10,10 +12,21 @@ public class statementOfBenefitsHomePage_stepDefinitions {
     @Given("user on VA staging environment for statement of benefits")
     public void user_on_va_staging_environment_for_statement_of_benefits() {
         Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+loginPage loginPage = new loginPage();
+loginPage.IDme.click();
+
 
     }
     @When("user signs in to staging environment statement of benefits")
     public void user_signs_in_to_staging_environment_statement_of_benefits() {
+        loginPage loginPage = new loginPage();
+        loginPage.emailInput.click();
+        loginPage.emailInput.sendKeys("vets.gov.user+0@gmail.com");
+        loginPage.passwordInput.click();
+        loginPage.passwordInput.sendKeys("369SsNrLgPv5");
+
+
+
 
     }
     @Then("user is able to see their statement of Benefits page")
