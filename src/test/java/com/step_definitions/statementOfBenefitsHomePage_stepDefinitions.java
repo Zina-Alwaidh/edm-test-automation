fb394bba02bfe9e06007d752ef8a4e42c94450e0
OITHOUAlWaiZ <zina.alwaidh@va.gov>
2024-10-09 15:08:11 -0500

    step definitions added to loginPage

--- src/test/java/com/step_definitions/statementOfBenefitsHomePage_stepDefinitions.java
+++ src/test/java/com/step_definitions/statementOfBenefitsHomePage_stepDefinitions.java
@@ -2,13 +2,18 @@ package com.step_definitions;
 
 import com.github.javafaker.Faker;
 import com.pages.loginPage;
+import com.utlities.BrowserUtils;
 import com.utlities.ConfigurationReader;
 import com.utlities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class statementOfBenefitsHomePage_stepDefinitions {
+    private static final Logger log = LoggerFactory.getLogger(statementOfBenefitsHomePage_stepDefinitions.class);
+
     @Given("user on VA staging environment for statement of benefits")
     public void user_on_va_staging_environment_for_statement_of_benefits() {
         Driver.getDriver().get(ConfigurationReader.getProperty("env"));
@@ -22,8 +27,16 @@ loginPage.IDme.click();
         loginPage loginPage = new loginPage();
         loginPage.emailInput.click();
         loginPage.emailInput.sendKeys("vets.gov.user+0@gmail.com");
+
         loginPage.passwordInput.click();
         loginPage.passwordInput.sendKeys("369SsNrLgPv5");
+
+        BrowserUtils.waitFor(3);
+        loginPage.signInButton.click();
+        BrowserUtils.waitFor(3);
+        loginPage.continueButton.click();
+        BrowserUtils.waitFor(2);
+        loginPage.continueButton.click();
 
 
 
